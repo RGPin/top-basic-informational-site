87b7f9504b9301bfcb6594bb7eda13f50eaa1a92
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-09 15:33:27 +0800

    Better... kinda?

--- index.js
+++ index.js
@@ -3,60 +3,30 @@ const fs = require("node:fs");
 
 console.log("test");
 
-const paths = ["/", "/about", "/contact-me"];
+const routes = {
+  "/": "index.html",
+  "/about": "about.html",
+  "/contact-me": "contact-me.html",
+};
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("./index.html", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Server Error");
-        return;
-      }
+  const file = routes[req.url] || "404.html";
 
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  }
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Server Error");
+      return;
+    }
 
-  if (req.url === "/about") {
-    fs.readFile("./about.html", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Server Error");
-        return;
-      }
+    if (file === "404.html") {
+      res.writeHead(404, { "Content-Type": "text/html" });
+    } else {
+      res.writeHead(res.statusCode || 200, { "Content-Type": "text/html" });
+    }
 
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  }
-
-  if (req.url === "/contact-me") {
-    fs.readFile("./contact-me.html", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Server Error");
-        return;
-      }
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  }
-
-  if (!paths.includes(req.url)) {
-    fs.readFile("./404.html", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Server Error");
-        return;
-      }
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  }
+    res.end(data);
+  });
 });
 
 server.listen(8080);
